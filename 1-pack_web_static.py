5d1b0874c956e187f577c5880ec72e87797f455b
Nick Nyanjui <nicknyanjui@gmail.com>
2022-10-27 21:56:41 +0000

    Add 1-pack_web_static

---
+++ 1-pack_web_static.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Fabric script that generates a .tgz archive
+    from the contents of the web_static folder
+"""
+from fabric.api import *
+import os
+
+
+def do_pack():
+    """ Compress files into tar archive """
+    v = local("mkdir -p versions")
+    result = local("tar -cvzf versions/web_static_$(date + '%Y%m%d%H%M%S').tgz web_static")
+    if result.stdout == 0:
+        return None
+    return os.path.realpath('versions')
+
