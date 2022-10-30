45ba6fc59c97fb1a116fe3a4b4489b5f6fc07b81
root <root@sandbox>
2022-10-30 10:45:26 +0000

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # Sets up your web servers for the deployment of web_static
-if [ ! "$(which nginx)" ]
+if [ ! "$(command -v nginx)" ]
 then
 	sudo apt-get update -y > /dev/null 2>&1 &&\
 	sudo apt-get install nginx -y > /dev/null 2>&1
 fi
 
-mkdir -p /data/web_static/{releases/test,shared,current}
+sudo mkdir -p /data/web_static/{releases/test,shared,current}
 
 HTML="<html>
   <head>
@@ -17,18 +17,19 @@ HTML="<html>
 </html>"
 echo -e "$HTML" | sudo tee /data/web_static/releases/test/index.html > /dev/null 2>&1
 
-if [ "$(find -L /data/web_static -maxdepth 1 -name current -xtype l | wc -l )" -gt 0 ]
+if [ "$(find -L /data/web_static/current -maxdepth 1 -xtype l | wc -l )" -gt 0 ]
 then
-	unlink /data/web_static/current
+	sudo unlink /data/web_static/current/index.html
 fi
-ln -ns /data/web_static/releases/test /data/web_static/current
+sudo ln -ns /data/web_static/releases/test/index.html /data/web_static/current
 
 if [ ! "$(id -u ubuntu)" ]
 then
 	sudo adduser ubuntu
 	sudo usermod -aG sudo,ubuntu ubuntu
 fi
-chown -R ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
+
 SERVER_CONFIG=\
 "server {
         listen 80 default_server;
@@ -39,22 +40,27 @@ SERVER_CONFIG=\
 
         root /var/www/html;
         index index.html index.htm index.nginx-debian.html;
-        try_files $uri $uri/ /index.html;
+        try_files \$uri \$uri/ /index.html;
 
         location /hbnb_static/ {
-                alias /var/www/static/;
+                alias /data/web_static/current/;
                 index index.html index.htm index.nginx-debian.html;
-                try_files $uri $uri/ /hbnb_static/index.html;
+                try_files \$uri \$uri/ /hbnb_static/index.html;
         }
-
         rewrite ^/redirect_me https://github.com/n1klaus permanent;
-        error_page 400 401 402 403 404 /40x.html;
+	error_page 400 401 402 403 404 /40x.html;
         error_page 500 502 503 504 /50x.html;
-        add_header X-Served-By $HOSTNAME;
+        add_header X-Served-By \$HOSTNAME;
         ignore_invalid_headers on;
 }"
 echo -e "$SERVER_CONFIG" | sudo tee /etc/nginx/sites-enabled/default > /dev/null 2>&1
 
-sudo nginx -t > /dev/null 2>&1 &&\
-sudo nginx -s reload > /dev/null 2>&1
+if [ "$(pgrep -c nginx)" -eq 0 ];
+then
+        sudo nginx -t > /dev/null 2>&1 &&\
+        sudo service nginx start
+else
+        sudo nginx -t > /dev/null 2>&1 &&\
+        sudo service nginx restart
+fi
 exit 0
