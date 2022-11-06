b7752190258bd9bcc268cf8c429ca1cd17772f22
Nick Nyanjui <nicknyanjui@gmail.com>
2022-11-06 08:36:18 +0000

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,16 +2,25 @@
 """ Fabric script that generates a .tgz archive
     from the contents of the web_static folder
 """
-from fabric import *
+from fabric.api import *
 import os
 
 
 def do_pack():
-    """ Compress files into tar archive """
-    v = local("mkdir -p versions")
-    result = local(
-        "tar -cvzf versions/web_static_$(date + '%Y%m%d%H%M%S').tgz\
-        web_static")
-    if result.stdout == 0:
+    """
+       Compress files into tar archive
+
+       Returns: the archive path if the archive has been correctly generated,
+                otherwise None
+    """
+    local("mkdir -p versions")
+    _date = local("echo $(date +'%Y%m%d%H%M%S')", capture=True)
+    _file = "web_static_{0}.tgz".format(_date.stdout)
+    output = local("tar -cvzf versions/{0} web_static".format(_file))
+    if output.failed:
         return None
-    return os.path.realpath('versions')
+    file_info = local("ls -l versions/{0}".format(_file), capture=True)
+    file_info = str(file_info.stdout).split(' ')
+    print("web_static packed: versions/{0} -> {1}Bytes".format(_file, file_info[4]))
+    env.archive_path = os.path.realpath("{0}".format(_file))
+    return env.archive_path
--- autopep8.sh
+++ autopep8.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env bash
 # Fix python scripts styling with autopep8 guidelines and pycodestyle style guide
 VENV='./start/*'
-if [ "$(/usr/bin/which autopep8 | wc -l)" == 0 ]
+
+if [ ! "$(command -v autopep8)" ]
 then
-    echo "missing package, start installing autopep8..."
+    echo "Missing package, start installing autopep8..."
     sudo apt-get update -y > /dev/null 2>&1 &&\
-	sudo apt-get install autopep8 -y > /dev/null 2>&1
+    sudo apt-get install autopep8 -y > /dev/null 2>&1
 fi
 
 echo "Running autopep..."
 find . -type f -name '*.py' ! -path '*/migrations/*' ! -path "$VENV" -exec \
     autopep8 --in-place --aggressive --recursive --verbose '{}' \;
 
-if [ "$(/usr/bin/which pycodestyle | wc -l)" == 0 ]
+if [ ! "$(command -v pycodestyle)" ]
 then
-    echo "missing package, start installing pycodestyle..."
+    echo "Missing package, start installing pycodestyle..."
     sudo apt-get update -y > /dev/null 2>&1 &&\
-	sudo apt-get install autopep8 -y > /dev/null 2>&1
+    sudo apt-get install autopep8 -y > /dev/null 2>&1
 fi
 
 echo -e "\n\nRunning pycodestyle..."
