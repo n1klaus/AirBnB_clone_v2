20c3c51df665b272936333711b428b85e5f47f13
Nick Nyanjui <nicknyanjui@gmail.com>
2022-11-13 11:17:31 +0300

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,15 @@
-""" """
+""" Module to start a Flask web application """
 from flask import Flask
 
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def home():
+    """ Function to define route to '/' """
     return f"Hello HBNB!"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=True)
